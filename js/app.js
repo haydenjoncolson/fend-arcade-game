48a8cfbc084c5de86938c82f76d0b8387455ec97
Hayden Jon <haydenjoncolson@gmail.com>
2016-11-28 13:13:20 -0800

    Change class Player to player

--- js/app.js
+++ js/app.js
@@ -24,15 +24,15 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+var player = function() {
     this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.update = function(dt) {
+player.prototype.update = function(dt) {
 
 };
 
-Player.prototype.render = function(dt) {
+player.prototype.render = function(dt) {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
